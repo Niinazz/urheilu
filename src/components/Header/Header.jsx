ff942c9d85c781b55fed09d9a8649bc2b54c4b28
Niina <ninjazz92@gmail.com>
2025-02-10 14:09:41 +0200

    lisää Header-komponentin

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.scss'
+
+
+
+function Header() {
+
+  return (
+    <div className={styles.header}>
+      Urheilupäiväkirja
+    </div>
+  )
+
+}
+
+export default Header
+
