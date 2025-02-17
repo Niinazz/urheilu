e8016957d80840dd5680657335e6bdfea984de39
Niina <ninjazz92@gmail.com>
2025-02-17 17:06:16 +0200

    lisää lomakekentät

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -3,8 +3,49 @@ import styles from './ItemForm.module.scss'
 function ItemForm(props) {
 
   return (
+
     <div>
-      Lomake
+        <form>
+        <div className={styles.itemform}>
+                    <div className={styles.itemform_row}>
+            <div>
+            <label htmlFor='type'>Urheilutyyppi</label>
+              <select name='type'>
+                <option>Juoksu</option>
+                <option>Uinti</option>
+                <option>Kuntosali</option>
+                <option>Kävely</option>
+              </select>
+
+            </div>
+          </div>
+          <div className={styles.itemform_row}>
+            <div>
+              <label htmlFor='amount'>Urheilun kesto</label>
+              <input type='number' name='amount' step='0.01' />
+            </div>
+            <div>
+              <label htmlFor='Date'>Urheilupäivä</label>
+              <input type='date' name='Date' />
+            </div>
+          </div>
+          <div className={styles.itemform_row}>
+            <div>
+              <label htmlFor='periodStart'>Urheilukuukauden alku</label>
+              <input type='date' name='periodStart' />
+            </div>
+            <div>
+              <label htmlFor='periodEnd'>Urheilukuukauden loppu</label>
+              <input type='date' name='periodEnd' />
+            </div>
+          </div>
+          <div className={styles.itemform_row}>
+            <div>
+          </div>    
+        </div>
+        </div>
+        </form>
+
     </div>
   )
 
