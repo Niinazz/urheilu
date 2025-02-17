e11ea93fe498ca893e28462c5386c6a760dbdbf1
Niina <ninjazz92@gmail.com>
2025-02-17 16:10:30 +0200

    lisää päivämäärämuotoilun Item-komponentille

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,13 +4,16 @@ import { MdNavigateNext } from 'react-icons/md'
 
   
 function Item({data, ...props}) {
+  const locale = "fi-FI"
+  const sportDate = new Date(data.Date).toLocaleDateString(locale)
+
 
         return (
             <div className={styles.item}>
               <div className={styles.item_data}>
               <div className={styles.item_type}>{data.type}</div>
               <div className={styles.item_amount}>{data.amount} min</div>
-              <div className={styles.item_date}>{data.Date}</div>
+              <div className={styles.item_date}>{sportDate}</div>
               <div className={styles.item_timespan}>{data.periodStart} - {data.periodEnd}</div>
               <div className={styles.item_average}>min/kk</div>
               </div>
