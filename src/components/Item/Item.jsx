078dea17bc5a3962c462fc05cd6ceec9c1f84dd8
Niina <ninjazz92@gmail.com>
2025-02-17 16:36:55 +0200

    lisää keskimääräisen kuukausikulun laskennan

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -7,15 +7,31 @@ function Item({data, ...props}) {
   const locale = "fi-FI"
   const sportDate = new Date(data.Date).toLocaleDateString(locale)
 
+  const numberFormat = new Intl.NumberFormat
+  const amount = numberFormat.format(data.amount)
+
+
+
+  let average
+  let period 
+  if (data.periodStart && data.periodEnd) {
+    const periodStart = new Date(data.periodStart)
+    const periodEnd = new Date(data.periodEnd)
+    period = periodStart.toLocaleDateString(locale) + " - " + periodEnd.toLocaleDateString(locale)
+    const days = (periodEnd - periodStart) / (24*60*60*1000)
+    average = numberFormat.format(data.amount / days * 30)
+  }
+
+
 
         return (
             <div className={styles.item}>
               <div className={styles.item_data}>
               <div className={styles.item_type}>{data.type}</div>
-              <div className={styles.item_amount}>{data.amount} min</div>
+              <div className={styles.item_amount}>{amount}</div>
               <div className={styles.item_date}>{sportDate}</div>
-              <div className={styles.item_timespan}>{data.periodStart} - {data.periodEnd}</div>
-              <div className={styles.item_average}>min/kk</div>
+              <div className={styles.item_timespan}>{period}</div>
+              <div className={styles.item_average}>{average ? average + "min/kk" : ""}</div>
               </div>
               <div className={styles.item_edit}>
         <MdNavigateNext />
