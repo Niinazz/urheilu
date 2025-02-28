b3fc12c8ab8b8303840bae515123d049c9898f1b
Niina <ninjazz92@gmail.com>
2025-02-28 14:30:03 +0200

    lisää urheilutyyppien lisäämisen Firestore-tietokantaan

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,35 +2,44 @@ import AppRouter from '../AppRouter'
 import { useEffect } from 'react'
 import useLocalStorage from '../../shared/uselocalstorage'
 import firebase from './firebase.js'
-import { collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query, setDoc  } from 'firebase/firestore'
-
+import { addDoc, collection, deleteDoc, doc, getFirestore, onSnapshot, orderBy, query } from 'firebase/firestore'
 
 const App = () => {
   const [data, setData] = useLocalStorage('urheilu-data', [])
   const [typelist, setTypelist] = useLocalStorage('urheilu-typelist', [])
 
-  useEffect( () => {
-    const unsubscribe = onSnapshot(query(collection(firestore,'item'),
-                                         orderBy('Date', 'desc')),
-                                   snapshot => {
-      const newData = []
-      snapshot.forEach( doc => {
-        newData.push({ ...doc.data(), id: doc.id })
-      })
-      setData(newData)
-    })
-    return unsubscribe
-  }, [])
+  const firestore = getFirestore(firebase)  // Määritellään firestore täällä
 
-  const handleTypeSubmit = (type) => {
-    const copy = [...typelist, type].sort()
-    setTypelist(copy)
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(firestore, 'type'), orderBy('type')),
+      (snapshot) => {
+        const newTypelist = []
+        snapshot.forEach((doc) => {
+          newTypelist.push(doc.data().type)
+        })
+        setTypelist(newTypelist)
+      }
+    )
+
+    return unsubscribe  // Palautetaan unsubscribe, jotta se puhdistuu, kun komponentti poistuu
+  }, [firestore])  // `firestore` lisätään riippuvuuksiin
+
+  const handleTypeSubmit = async (type) => {
+    try {
+      await addDoc(collection(firestore, 'type'), { type: type })
+    } catch (error) {
+      console.error('Error adding type: ', error)
+    }
   }
 
   const handleItemDelete = async (id) => {
-    await deleteDoc(doc(firestore, 'item', id))
+    try {
+      await deleteDoc(doc(firestore, 'item', id))
+    } catch (error) {
+      console.error('Error deleting item: ', error)
+    }
   }
-
 
   const handleItemSubmit = (newitem) => {
     const copy = [...data]
@@ -43,7 +52,7 @@ const App = () => {
     }
 
     copy.sort((a, b) => new Date(b.Date) - new Date(a.Date))
-    setData([...copy]) // Uusi taulukko Reactin tilapäivityksen varmistamiseksi
+    setData([...copy])  // Varmistetaan uuden taulukon asettaminen
   }
 
   return (
@@ -58,3 +67,4 @@ const App = () => {
 }
 
 export default App
+
